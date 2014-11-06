95ca05f164af0fb886ebb188615c275d02233e40
sauliuni <sauli@informatik.uni-bremen.de>
2014-11-06 15:52:26 +0100

    usage of controllers

--- main.js
+++ main.js
@@ -2,14 +2,97 @@ var app = angular.module('twitterApp', []);
 app.controller("AppCtrl", function($scope){
     $scope.loadMoreTweets = function(){
         alert("Laoding tweets");
-    }
+    };
     $scope.deleteTweets = function(){
         alert("deleting");
-    }
-}).directive("enter", function(){
-    return function(scope, element, attrs){
-        element.bind("mouseenter", function(){
-            scope.$apply(attrs.enter);
-        })
-    }
-});
+    };
+    $scope.expanders = [
+        {
+            title : "Eins",
+            text: "Hallo eins"
+        },
+        {
+            title : "zwei",
+            text: "Hallo zwei"
+        },
+        {
+            title : "drei",
+            text: "Hallo drei"
+        }];
+}).directive("superhero", function(){
+    return {
+        restrict : "E",
+        scope:{},
+        controller: function ($scope) {
+            $scope.abilities = [];
+            this.addStength = function (){
+                $scope.abilities.push("strength")
+            };
+            this.addSpeed = function(){
+                $scope.abilities.push("speed");
+            };
+            this.addFlight = function(){
+                $scope.abilities.push("flight");
+            };
+        },
+        link : function (scope, element) {
+            element.bind("mouseenter", function(){
+                console.log(scope.abilities);
+            })
+        }
+    }})
+    .directive("speed", function(){
+    return {
+        require: "^superhero",
+        link: function (scope, element, attr, superheroCtrl) {
+            superheroCtrl.addSpeed();
+            console.log('added speed');
+            element.bind("mouseenter", function(){
+                superheroCtrl.addSpeed();
+                console.log("now added really");
+            });
+        }
+    }})
+    .directive("showDetails", function () {
+        return {
+            restrict: "EA",
+            scope: {
+                title : "@detailsTitle"
+            },
+            require: "^accordion",
+            replace: true,
+            transclude: true,
+            template: '<div><div class="title" ng-click="toggle()">{{title}}</div><div class="body" ng-show="shouldShow" ng-transclude></div></div>',
+            link: function(scope, element, attr, controllers){
+                scope.shouldShow = false;
+                controllers.addExpander(scope);
+                scope.toggle = function() {
+                    scope.shouldShow = !scope.shouldShow;
+                };
+            }
+        }
+
+    })
+    .directive("accordion", function () {
+        return {
+            restrict: "EA",
+            replace: true,
+            transclude: true,
+            template: "<div ng-transclude></div>",
+            controller: function () {
+                var expanders = [];
+                console.log("accordion init");
+                this.gotOpened = function(selectedItem){
+                    angular.forEach(expanders, function(expander){
+                        if (selectedItem = !expander){
+                            expander.shouldShow = false;
+                        }
+                    });
+                };
+                this.addExpander = function(expander){
+                    expanders.push(expander);
+
+                }
+            }
+        }
+    });
